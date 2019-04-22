b056f6198c485d274f4470732a9f57e42c055eb9
Mahendra Yadav <mahendra.k12@gmail.com>
2019-04-22 17:16:24 +0530

    Finished exercise 3

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,11 +1,51 @@
 import React from 'react';
 
-function Courses() {
-  return (
-    <div>
-      <h2>Courses</h2>
-    </div>
-  );
+class Courses extends React.Component {
+	constructor(props){
+		super(props);
+		this.state = {courses: [], value: ''};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
+	handleChange(event) {
+		this.setState({value: event.target.value})
+	}
+
+	handleSubmit(event) {
+		this.setState({courses: [...this.state.courses, this.state.value]});
+
+		event.preventDefault();
+		this.setState({value: ''})
+	}
+	render() {
+		return (
+			<div>
+				<h2>Courses</h2>
+				<ul>
+					{this.state.courses.map((course, index) => <Course course={course} key={index} />)}
+				</ul>
+
+				<h3>Add Courses</h3>
+				<form onSubmit={this.handleSubmit}>
+					<div class="form-group col-sm-4">
+						<input class="form-control" type="text" name="course" value={this.state.value} onChange={this.handleChange}/>
+					</div>
+					<div class="form-group col-sm-2">
+						<input type="submit" value="save" class="btn btn-primary mb-2"/>
+					</div>
+				</form>
+			</div>
+		)
+	}
+}
+
+
+class Course extends React.Component {
+	render() {
+		return <li>{this.props.course}</li>
+	}
 }
 
 export default Courses;
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,11 +5,11 @@ import { NavLink } from 'react-router-dom';
 function Header() {
 	return(
 		<nav className="nav">
-				<NavLink exact to="/" className="nav-link" activeStyle={{color: "orange"}} activeClassName>Home</NavLink>
-						<span class="nav-link sep">|</span>
-				<NavLink to="/courses" className="nav-link" activeStyle={{color: "orange"}} activeClassName>Courses</NavLink>
-						<span class="nav-link sep">|</span>
-				<NavLink to="/about" className="nav-link" activeStyle={{color: "orange"}} activeClassName>About</NavLink>
+				<NavLink exact to="/" className="nav-link" activeStyle={{color: "orange"}} activeClassName="active">Home</NavLink>
+						<span className="nav-link sep">|</span>
+				<NavLink to="/courses" className="nav-link" activeStyle={{color: "orange"}} activeClassName="active">Courses</NavLink>
+						<span className="nav-link sep">|</span>
+				<NavLink to="/about" className="nav-link" activeStyle={{color: "orange"}} activeClassName="active">About</NavLink>
 		</nav>
 	);
 }
